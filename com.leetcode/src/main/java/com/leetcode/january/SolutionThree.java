a50722718b0ed9b5c256debefab3b9365255f1ae
wanglong16 <wanglong16@meicai.cn>
2021-01-28 23:55:07 +0800

    merge

--- com.leetcode/src/main/java/com/leetcode/january/SolutionThree.java
+++ com.leetcode/src/main/java/com/leetcode/january/SolutionThree.java
@@ -115,4 +115,34 @@ public class SolutionThree {
         return max;
     }
 
+    /**
+     * 88. 合并两个有序数组
+     * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+     *
+     * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+     * 示例 1：
+     *
+     * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+     * 输出：[1,2,2,3,5,6]
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        //遍历nums2 比较nums1并插入
+        int cursor = 0;
+        for (int i = 0; i < n; i ++) {
+            int cur = nums2[n];
+            for (int j = cursor; j < m + i; j++) {
+                if (nums1[j] > cur) {
+                    //记下nums1第一个大于当前数的位置
+                    cursor = j;
+                    // j之后的数据后移
+                    for (int k = m + i; k > j; k--) {
+                        nums1[k+1] = nums1[k];
+                    }
+                    nums1[j] = cur;
+                    break;
+                }
+            }
+        }
+    }
+
 }
