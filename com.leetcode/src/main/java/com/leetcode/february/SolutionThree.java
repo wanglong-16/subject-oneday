ead3d1d3083584f8ecb30bb187e8140be772f9c3
wanglong16 <wanglong16@meicai.cn>
2021-02-06 11:45:21 +0800

    day 6

--- com.leetcode/src/main/java/com/leetcode/february/SolutionThree.java
+++ com.leetcode/src/main/java/com/leetcode/february/SolutionThree.java
@@ -1,6 +1,8 @@
 package com.leetcode.february;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -62,5 +64,81 @@ public class SolutionThree {
             n /= 10;
         }
         return ret;
+    }
+
+    /**
+     * 66. 加一
+     * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+     *
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     *
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：digits = [1,2,3]
+     * 输出：[1,2,4]
+     * 解释：输入数组表示数字 123。
+     */
+    public int[] plusOne(int[] digits) {
+        boolean nextOne = false;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (i == 0 && digits[0] == 9 && nextOne || digits.length == 1 && digits[0] == 9) {
+                int [] ret = new int [digits.length + 1];
+                ret[0] = 1;
+                return ret;
+            }
+            int curr = digits[i];
+            if (nextOne || i == digits.length - 1) {
+                curr ++;
+                nextOne = curr > 9;
+                digits [i] = curr % 10;
+            }
+        }
+        return digits;
+    }
+
+    /**
+     * 345. 反转字符串中的元音字母
+     * 编写一个函数，以字符串作为输入，反转该字符串中的元音字母。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入："hello"
+     * 输出："holle"
+     * 示例 2：
+     *
+     * 输入："leetcode"
+     * 输出："leotcede"
+     */
+    public String reverseVowels(String s) {
+        char [] chars = s.toCharArray();
+        List<Character> characters = Arrays.asList('a','e','i','o','u','A','E','I','O','U'
+);
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            if (characters.contains(chars[left]) && characters.contains(chars[right])) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left ++;
+                right --;
+            }
+            if (!characters.contains(chars[left])) {
+                left ++;
+            }
+            if (!characters.contains(chars[right])) {
+                right --;
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Character character : chars) {
+            stringBuilder.append(character);
+        }
+        return stringBuilder.toString();
     }
 }
