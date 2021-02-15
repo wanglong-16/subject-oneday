66ba11ee6d8fcc8947a702f352a010ca190af84b
wanglong16 <wanglong16@meicai.cn>
2021-02-15 09:06:33 +0800

    day 14

--- com.leetcode/src/main/java/com/leetcode/february/SolutionSeven.java
+++ com.leetcode/src/main/java/com/leetcode/february/SolutionSeven.java
@@ -139,4 +139,19 @@ public class SolutionSeven {
         }
         return Math.max(maxConsecutiveOnes, pNext - pStart);
     }
+
+    public int findMaxConsecutiveOnesV1(int[] nums) {
+        int maxCount = 0, count = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                maxCount = Math.max(maxCount, count);
+                count = 0;
+            }
+        }
+        maxCount = Math.max(maxCount, count);
+        return maxCount;
+    }
 }
