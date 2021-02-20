ba7789b6b53e84ac659de8fd0354680bda356228
wanglong16 <wanglong16@meicai.cn>
2021-02-20 23:41:51 +0800

    day20

--- com.leetcode/src/main/java/com/leetcode/february/SolutionEleven.java
+++ com.leetcode/src/main/java/com/leetcode/february/SolutionEleven.java
@@ -90,6 +90,41 @@ public class SolutionEleven {
         return m << count;
     }
 
-
+    /**
+     * 405. 数字转换为十六进制数
+     * 给定一个整数，编写一个算法将这个数转换为十六进制数。对于负整数，我们通常使用 补码运算 方法。
+     * 注意:
+     * 十六进制中所有字母(a-f)都必须是小写。
+     * 十六进制字符串中不能包含多余的前导零。如果要转化的数为0，那么以单个字符'0'来表示；对于其他情况，十六进制字符串中的第一个字符将不会是0字符。
+     * 给定的数确保在32位有符号整数范围内。
+     * 不能使用任何由库提供的将数字直接转换或格式化为十六进制的方法。
+     * 示例 1：
+     * 输入:
+     * 26
+     * 输出:
+     * "1a"
+     * 示例 2：
+     * 输入:
+     * -1
+     * 输出:
+     * "ffffffff"
+     */
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        Map<Integer, String> hexMap = new HashMap<Integer, String>() {{
+            put(0, "0");put(1, "1"); put(2, "2"); put(3, "3");
+            put(4, "4");put(5, "5"); put(6, "6"); put(7, "7");
+            put(8, "8");put(9, "9"); put(10, "a"); put(11, "b");
+            put(12, "c");put(13, "d"); put(14, "e"); put(15, "f");
+        }};
+        StringBuilder stringBuilder = new StringBuilder();
+        while (num != 0) {
+            stringBuilder.append(hexMap.get(num & 0xf));
+            num >>>= 4;
+        }
+        return stringBuilder.reverse().toString();
+    }
 
 }
