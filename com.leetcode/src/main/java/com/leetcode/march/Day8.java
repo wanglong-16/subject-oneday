286be3409a1aefeda8c947b9a69fd49cc3d8abd2
wanglong16 <wanglong16@meicai.cn>
2021-03-10 09:07:27 +0800

    day9

--- com.leetcode/src/main/java/com/leetcode/march/Day8.java
+++ com.leetcode/src/main/java/com/leetcode/march/Day8.java
@@ -2,6 +2,9 @@ package com.leetcode.march;
 
 import com.leetcode.util.linked.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @description:
  * @version: 1.0
@@ -71,21 +74,177 @@ public class Day8 {
     }
 
     public boolean isPalindrome(ListNode head) {
-        ListNode prev = null, temp = head;
+        ListNode prev = null, node = new ListNode(0);
+        ListNode temp = node;
         while (head != null) {
+            temp.next = head;
+            temp = temp.next;
             ListNode nextTemp = head.next; // nextT = 2 3 4 5 => 3 4 5
             head.next = prev; //head = 1 => 2 1
             prev = head; // prev = 1 => 2 1
             head = nextTemp; // head = 2 3 4 5 => 3 4 5
         }
-        while (temp != null) {
-            if (temp.val != prev.val) {
+        node = node.next;
+        while (node != null) {
+            if (node.val != prev.val) {
                 return false;
             }
             prev = prev.next;
-            temp = temp.next;
+            node = node.next;
         }
         return true;
     }
 
+    /**
+     * 328. 奇偶链表
+     * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+     *
+     * 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+     *
+     * 示例 1:
+     *
+     * 输入: 1->2->3->4->5->NULL
+     * 输出: 1->3->5->2->4->NULL
+     * 示例 2:
+     *
+     * 输入: 2->1->3->5->6->4->7->NULL
+     * 输出: 2->3->6->7->1->5->4->NULL
+     * 说明:
+     *
+     * 应当保持奇数节点和偶数节点的相对顺序。
+     * 链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+     */
+    public ListNode oddEvenListV1(ListNode head) {
+        ListNode oddHead = new ListNode(0);
+        ListNode evenHead = new ListNode(0);
+        ListNode oddTemp = oddHead, evenTemp = evenHead;
+        int count = 1;
+        while (head != null) {
+            if (count % 2 == 0) {
+                evenTemp.next = new ListNode(head.val);
+                evenTemp = evenTemp.next;
+            } else {
+                oddTemp.next = new ListNode(head.val);
+                oddTemp = oddTemp.next;
+            }
+            head = head.next;
+            count ++;
+        }
+        oddTemp.next = evenHead.next;
+        return oddHead.next;
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode evenHead = head.next;
+        ListNode odd = head, even = evenHead;
+        while (even != null && even.next != null) {
+            //使得节点分离， 奇偶节点分离
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA, B = headB;
+        while (A != B) {
+            A = A == null ? headB : A.next;
+            B = B == null ? headA : B.next;
+        }
+        return A;
+    }
+
+    /**
+     * 725. 分隔链表
+     * 给定一个头结点为 root 的链表, 编写一个函数以将链表分隔为 k 个连续的部分。
+     * 每部分的长度应该尽可能的相等: 任意两部分的长度差距不能超过 1，也就是说可能有些部分为 null。
+     * 这k个部分应该按照在链表中出现的顺序进行输出，并且排在前面的部分的长度应该大于或等于后面的长度。
+     *
+     * 返回一个符合上述规则的链表的列表。
+     *
+     * 举例： 1->2->3->4, k = 5 // 5 结果 [ [1], [2], [3], [4], null ]
+     *
+     * 示例 1：
+     *
+     * 输入:
+     * root = [1, 2, 3], k = 5
+     * 输出: [[1],[2],[3],[],[]]
+     * 解释:
+     * 输入输出各部分都应该是链表，而不是数组。
+     * 例如, 输入的结点 root 的 val= 1, root.next.val = 2, \root.next.next.val = 3, 且 root.next.next.next = null。
+     * 第一个输出 output[0] 是 output[0].val = 1, output[0].next = null。
+     * 最后一个元素 output[4] 为 null, 它代表了最后一个部分为空链表。
+     * 示例 2：
+     *
+     * 输入:
+     * root = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k = 3
+     * 输出: [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
+     * 解释:
+     * 输入被分成了几个连续的部分，并且每部分的长度相差不超过1.前面部分的长度大于等于后面部分的长度。
+     * 提示:
+     *
+     * root 的长度范围： [0, 1000].
+     * 输入的每个节点的大小范围：[0, 999].
+     * k 的取值范围： [1, 50].
+     */
+    public ListNode[] splitListToPartsV1(ListNode root, int k) {
+        List<ListNode> nodeList = new ArrayList<>();
+        while (root != null) {
+            nodeList.add(root);
+            root = root.next;
+        }
+        // 共 10个 k = 3 => 4 3 3
+        int batch = nodeList.size() / k; // 10 / 3 = 3
+        int rem = nodeList.size() % k; // 10 % 3 = 3 余 1
+        ListNode [] ans = new ListNode[batch];
+        for (int i = 0; i < batch; i++) {
+            if (i < rem) {
+                ListNode at = new ListNode(0);
+                ListNode aTemp = at;
+                for (ListNode n : nodeList.subList(i * batch, (i + 1) * batch + 1)) {
+                    aTemp.next = new ListNode(n.val);
+                    aTemp = aTemp.next;
+                }
+                ans[i] = at.next;
+            } else {
+                ListNode bt = new ListNode(0);
+                ListNode bTemp = bt;
+                for (ListNode n : nodeList.subList(i * batch + rem, (i + 1) * batch + rem)) {
+                    bTemp.next = new ListNode(n.val);
+                    bTemp = bTemp.next;
+                }
+                ans[i] = bt.next;
+            }
+        }
+        return ans;
+    }
+
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        ListNode cur = root;
+        int N = 0;
+        while (cur != null) {
+            cur = cur.next;
+            N++;
+        }
+        int width = N / k, rem = N % k;
+        ListNode[] ans = new ListNode[k];
+        cur = root;
+        for (int i = 0; i < k; ++i) {
+            ListNode head = new ListNode(0), write = head;
+            for (int j = 0; j < width + (i < rem ? 1 : 0); ++j) {
+                write = write.next = new ListNode(cur.val);
+                if (cur != null) {
+                    cur = cur.next;
+                }
+            }
+            ans[i] = head.next;
+        }
+        return ans;
+    }
 }
