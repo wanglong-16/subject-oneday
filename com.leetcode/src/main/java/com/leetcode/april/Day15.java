f31fd36e82ccc9e7ea27d5d37581f8549d55ae48
wanglong16 <wanglong16@meicai.cn>
2021-04-16 10:26:36 +0800

    day 15

---
+++ com.leetcode/src/main/java/com/leetcode/april/Day15.java
@@ -0,0 +1,163 @@
+package com.leetcode.april;
+
+/**
+ * @description:
+ * @version: 1.0
+ * @date: 2021-04-15 08:09:05
+ * @author: wanglong16@meicai.cn
+ */
+public class Day15 {
+
+    /**
+     * 213. 打家劫舍 II
+     * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+     *
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，能够偷窃到的最高金额。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [2,3,2]
+     * 输出：3
+     * 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+     * 示例 2：
+     *
+     * 输入：nums = [1,2,3,1,4]
+     * 输出：4
+     * 解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+     *      偷窃到的最高金额 = 1 + 3 = 4 。
+     * 示例 3：
+     *
+     * 输入：nums = [0]
+     * 输出：0
+     *
+     *
+     * 提示：
+     *
+     * 1 <= nums.length <= 100
+     * 0 <= nums[i] <= 1000
+     */
+    public int rob(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+    }
+
+    public int robRange(int[] nums, int start, int end) {
+        int first = nums[start], second = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+    /**
+     * 1582. 二进制矩阵中的特殊位置
+     * 给你一个大小为 rows x cols 的矩阵 mat，其中 mat[i][j] 是 0 或 1，请返回 矩阵 mat 中特殊位置的数目 。
+     *
+     * 特殊位置 定义：如果 mat[i][j] == 1 并且第 i 行和第 j 列中的所有其他元素均为 0（行和列的下标均 从 0 开始 ），则位置 (i, j) 被称为特殊位置。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：mat = [[1,0,0],
+     *             [0,0,1],
+     *             [1,0,0]]
+     * 输出：1
+     * 解释：(1,2) 是一个特殊位置，因为 mat[1][2] == 1 且所处的行和列上所有其他元素都是 0
+     * 示例 2：
+     *
+     * 输入：mat = [[1,0,0],
+     *             [0,1,0],
+     *             [0,0,1]]
+     * 输出：3
+     * 解释：(0,0), (1,1) 和 (2,2) 都是特殊位置
+     * 示例 3：
+     *
+     * 输入：mat = [[0,0,0,1],
+     *             [1,0,0,0],
+     *             [0,1,1,0],
+     *             [0,0,0,0]]
+     * 输出：2
+     * 示例 4：
+     *
+     * 输入：mat = [[0,0,0,0,0],
+     *             [1,0,0,0,0],
+     *             [0,1,0,0,0],
+     *             [0,0,1,0,0],
+     *             [0,0,0,1,1]]
+     * 输出：3
+     *
+     *
+     * 提示：
+     *
+     * rows == mat.length
+     * cols == mat[i].length
+     * 1 <= rows, cols <= 100
+     * mat[i][j] 是 0 或 1
+     */
+    public int numSpecial(int[][] mat) {
+        int[] r = new int[mat.length], c = new int[mat[0].length];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 1) {
+                    r[i] ++;
+                    c[j] ++;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 1 && r[i] == 1 && c[j] == 1) {
+                    ans ++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 剑指 Offer 57. 和为s的两个数字
+     * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[2,7] 或者 [7,2]
+     * 示例 2：
+     *
+     * 输入：nums = [10,26,30,31,47,60], target = 40
+     * 输出：[10,30] 或者 [30,10]
+     *
+     *
+     * 限制：
+     *
+     * 1 <= nums.length <= 10^5
+     * 1 <= nums[i] <= 10^6
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = nums[i];
+                    ans[1] = nums[j];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
