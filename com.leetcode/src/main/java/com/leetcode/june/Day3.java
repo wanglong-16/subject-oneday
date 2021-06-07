cb2024752b8520ff25c3b95f472e28d3e68fb992
wanglong16 <wanglong16@meicai.cn>
2021-06-07 09:52:22 +0800

    day7

--- com.leetcode/src/main/java/com/leetcode/june/Day3.java
+++ com.leetcode/src/main/java/com/leetcode/june/Day3.java
@@ -133,8 +133,97 @@ public class Day3 {
         return n == 1;
     }
 
-    public static void main(String[] args) {
-
+    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        return strToNum(targetWord) == strToNum(firstWord) + strToNum(secondWord);
     }
+
+    private int strToNum(String str) {
+        StringBuilder sb = new StringBuilder();
+        boolean isFirst = true;
+        for (Character ch : str.toCharArray()) {
+            if (ch != 'a') {
+                isFirst = false;
+            }
+            if (!isFirst) {
+                sb.append((int) ch - 'a');
+            }
+        }
+        return sb.length() == 0 ? 0 : Integer.parseInt(sb.toString());
+    }
+
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int ans = 0;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(dp[i], ans);
+        }
+        return ans;
+    }
+
+    /**
+     * 494. 目标和
+     * 给你一个整数数组 nums 和一个整数 target 。
+     *
+     * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+     *
+     * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+     * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [1,1,1,1,1], target = 3
+     * 输出：5
+     * 解释：一共有 5 种方法让最终目标和为 3 。
+     * -1 + 1 + 1 + 1 + 1 = 3
+     * +1 - 1 + 1 + 1 + 1 = 3
+     * +1 + 1 - 1 + 1 + 1 = 3
+     * +1 + 1 + 1 - 1 + 1 = 3
+     * +1 + 1 + 1 + 1 - 1 = 3
+     * 示例 2：
+     *
+     * 输入：nums = [1], target = 1
+     * 输出：1
+     *
+     *
+     * 提示：
+     *
+     * 1 <= nums.length <= 20
+     * 0 <= nums[i] <= 1000
+     * 0 <= sum(nums[i]) <= 1000
+     * -1000 <= target <= 100
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays(int[] nums, int target) {
+        int total = 1 << nums.length, ans = 0, sign = 0, calculate = 0;
+        for (int i = 0; i < total; i++) {
+            sign = i;
+            calculate = 0;
+            for (int num : nums) {
+                if ((sign & 1) == 1) {
+                    calculate += num;
+                } else {
+                    calculate -= num;
+                }
+                sign >>= 1;
+            }
+            if (target == calculate) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
 
 }
