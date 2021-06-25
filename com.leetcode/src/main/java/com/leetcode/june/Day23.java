6c5ed232ec87f451b97060a60f01f777c8907e6f
wanglong16 <wanglong16@meicai.cn>
2021-06-25 19:00:01 +0800

    day25

--- com.leetcode/src/main/java/com/leetcode/june/Day23.java
+++ com.leetcode/src/main/java/com/leetcode/june/Day23.java
@@ -1,5 +1,7 @@
 package com.leetcode.june;
 
+import com.leetcode.util.tree.TreeNode;
+
 /**
  * @description:
  * @version: 1.0
@@ -90,6 +92,79 @@ public class Day23 {
                 dfs(board, row + d[0], col + d[1]);
             }
         }
+    }
+
+    /**
+     * 323. 无向图中连通分量的数目
+     * 给定编号从 0 到 n-1 的 n 个节点和一个无向边列表（每条边都是一对节点），请编写一个函数来计算无向图中连通分量的数目。
+     *
+     * 示例 1:
+     *
+     * 输入: n = 5 和 edges = [[0, 1], [1, 2], [3, 4]]
+     *
+     *      0          3
+     *      |          |
+     *      1 --- 2    4
+     *
+     * 输出: 2
+     * 示例 2:
+     *
+     * 输入: n = 5 和 edges = [[0, 1], [1, 2], [2, 3], [3, 4]]
+     *
+     *      0           4
+     *      |           |
+     *      1 --- 2 --- 3
+     *
+     * 输出:  1
+     * 注意:
+     * 你可以假设在 edges 中不会出现重复的边。而且由于所以的边都是无向边，[0, 1] 与 [1, 0]  相同，所以它们不会同时在 edges 中出现。
+     * @param n
+     * @param edges
+     * @return
+     */
+    public int countComponents(int n, int[][] edges) {
+        //todo
+        return 0;
+    }
+
+    public int sumOfLeftLeaves(TreeNode root) {
+
+        dfs(root);
+        return ans;
+    }
+
+    int ans = 0;
+
+    public void dfs(TreeNode root) {
+        if (root != null) {
+            if (root.left != null && root.left.left == null && root.left.right == null) {
+                ans += root.left.val;
+            } else {
+                dfs(root.left);
+                dfs(root.right);
+            }
+        }
+    }
+
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        boolean nag = num < 0;
+        int absNum = Math.abs(num);
+        StringBuilder ans = new StringBuilder();
+        while (absNum > 0) {
+            int t = absNum - (absNum / 7) * 7;
+            ans.append(t);
+            absNum = absNum / 7;
+        }
+        ans = nag ? ans.append("-") : ans;
+        return ans.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Day23 day23 = new Day23();
+        System.out.println(day23.convertToBase7(-7));
     }
 
 }
